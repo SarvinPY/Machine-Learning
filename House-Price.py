3ac22c73bdc96a128cf1f1009855fe07974d1eae
sarvin <151523715+SarvinPY@users.noreply.github.com>
2026-09-10 16:54:53 +0330

    Delete House-Price.py

--- House-Price.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("house-price.csv")
-# df[df['Address'].isnull()]
-df = df.dropna(subset=['Address'])
-print(df.isnull().sum())   # باید Address حالا 0 نشون بده
-print(len(df))             # تعداد ردیف‌های باقی‌مونده
